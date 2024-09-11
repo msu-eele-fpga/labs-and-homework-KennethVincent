24d022bcced7ab4d1ec9f2610cfc72b2cef3f8de
Kenneth Vincent <kennyda2001@gmail.com>
2024-09-11 05:08:10 -0600

    modifing the vhdl files in hw 4

--- sim/timed-counter/timed_counter_tb.vhd
+++ sim/timed-counter/timed_counter_tb.vhd
@@ -22,9 +22,13 @@ architecture testbench of timed_counter_tb is
 	end component timed_counter;
 
 	signal clk_tb : std_ulogic := '0';
+	signal clk_2_tb : std_ulogic := '0';
 
 	signal enable_100ns_tb : boolean := false;
 	signal done_100ns_tb   : boolean ;
+
+	signal enable_500ns_tb : boolean := false;
+	signal done_500ns_tb   : boolean ;
 
 	constant HUNDRED_NS : time := 100 ns;
 
@@ -102,8 +106,63 @@ begin
 					end loop;
 				end loop;
 				-- testbench is done :)
-				std.env.finish;
+				--std.env.finish;
 
 		end process stimuli_and_checker;
+
+	dut_500ns_counter : component timed_counter
+		generic map (
+			clk_period => CLK_PERIOD,
+			count_time => five_100
+		)
+		port map (
+			clk => clk_tb,
+			enable => enable_500ns_tb,
+			done => done_500ns_tb
+		);
+
+		clk_2_tb <= not clk_2_tb after CLK_PERIOD / 2;
+
+		stimuli_and_checker_2 : process is
+			begin
+				-- test 100 ns timer when it's enabled
+				print("testing 500 ns timer: enabled");
+				wait_for_clock_edge(clk_2_tb);
+				enable_500ns_tb <= true;
+
+				-- loop for the number of clock cycles that is equal to the timer's period
+				for i in 0 to (five_100 / CLK_PERIOD) loop
+					wait_for_clock_edge(clk_2_tb);
+					predict_counter_done(five_100, enable_500ns_tb, done_500ns_tb, i);
+
+				end loop;
+				
+				-- add other test cases here
+
+				print("testing 100 ns timer twice: disabled");
+				--test case two
+				wait_for_clock_edge(clk_2_tb);
+				enable_500ns_tb <= false;
+				for i in 0 to (2*(five_100 / CLK_PERIOD)) loop
+					wait_for_clock_edge(clk_2_tb);
+					predict_counter_done(five_100, enable_500ns_tb, done_500ns_tb, i);
+
+				end loop;
+
+				--test case three
+				print("testing 500 ns timer 4 times: enabled");
+				wait_for_clock_edge(clk_2_tb);
+				enable_500ns_tb <= true;
+				for j in 0 to 4 loop
+					for i in 0 to ((five_100 / CLK_PERIOD)) loop
+						wait_for_clock_edge(clk_2_tb);
+						predict_counter_done(five_100, enable_500ns_tb, done_500ns_tb, i);
+
+					end loop;
+				end loop;
+				-- testbench is done :)
+				std.env.finish;
+
+		end process stimuli_and_checker_2;
 
 end architecture; 
