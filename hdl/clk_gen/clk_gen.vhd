3430b7d33531af0861503e7b9d84300762924a8e
Kenneth Vincent <kennyda2001@gmail.com>
2024-09-27 15:08:55 -0600

    the next step foward for the 4th lab

--- hdl/clk_gen/clk_gen.vhd
+++ hdl/clk_gen/clk_gen.vhd
@@ -1,1 +1,42 @@
--- start of the clock generator
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.std_logic_textio.all;
+use work.assert_pkg.all;
+use work.print_pkg.all;
+use work.tb_pkg.all;
+
+entity clk_gen is
+	port (
+		clk         	   : in std_ulogic;
+		rst 	           : in std_ulogic;
+		base_period        : in std_ulogic;
+		half_base_period   : out std_ulogic;
+		x2_base_period     : out std_ulogic;
+		fourth_base_period : out std_ulogic;
+		eight_base_period  : out std_ulogic;
+		x4_base_period     : out std_ulogic;
+	);
+end entity clk_gen;
+
+architecture clock in clk_gen is
+
+	begin
+
+		new_rate : process (base_period)
+			begin
+				if(rst = '0') then
+					half_base_period <= '0'; 
+					x2_base_period <= '0';
+					fourth_base_period <= '0'; 
+					eight_base_period <= '0';
+					x4_base_period <= '0';
+				elsif(clk'event and clk = '1') then
+					half_base_period <= base_period ror 2; 
+					x2_base_period <= base_period rol 2;
+					fourth_base_period <= base_period ror 4; 
+					eight_base_period <= base_period ror 8;
+					x4_base_period <= base_period rol 4;
+				end if;
+		end process;
+end architecture clock;
--- hdl/pttn_gen/pttn_gen.vhd
+++ hdl/pttn_gen/pttn_gen.vhd
@@ -1,1 +1,70 @@
--- start of pattern generator
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.std_logic_textio.all;
+use work.assert_pkg.all;
+use work.print_pkg.all;
+use work.tb_pkg.all;
+
+
+entity pttn_gen is
+	port (
+		clk         	   : in std_ulogic;
+		rst 	           : in std_ulogic;
+		half_base_period   : in std_ulogic;
+		x2_base_period     : in std_ulogic;
+		fourth_base_period : in std_ulogic;
+		eight_base_period  : in std_ulogic;
+		x4_base_period     : in std_ulogic;
+		pttn0		   : out std_ulogic_vector(6 downto 0);
+		pttn1		   : out std_ulogic_vector(6 downto 0);
+		pttn2		   : out std_ulogic_vector(6 downto 0);
+		pttn3		   : out std_ulogic_vector(6 downto 0);
+		pttn4		   : out std_ulogic_vector(6 downto 0);
+	);
+end entity pttn_gen;
+
+architecture Patterns in pttn_gen is
+
+	begin
+
+		pattern0 : process (base_period)
+			begin
+				if(rst = '0') then
+				elsif(clk'event and clk = '1') then
+
+				end if;
+		end process;
+
+		pattern1 : process (base_period)
+			begin
+				if(rst = '0') then
+				elsif(clk'event and clk = '1') then
+
+				end if;
+		end process;
+
+		pattern2 : process (base_period)
+			begin
+				if(rst = '0') then
+				elsif(clk'event and clk = '1') then
+
+				end if;
+		end process;
+
+		pattern3 : process (base_period)
+			begin
+				if(rst = '0') then
+				elsif(clk'event and clk = '1') then
+
+				end if;
+		end process;
+
+		pattern4 : process (base_period)
+			begin
+				if(rst = '0') then
+				elsif(clk'event and clk = '1') then
+
+				end if;
+		end process;
+end architecture Patterns;
